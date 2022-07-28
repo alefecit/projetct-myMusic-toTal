cb425259961c23097f83c3406f493ff0296ddae2
aleferamos <alefepdias@gmail.com>
2022-07-28 09:30:34 -0400

    Implementando teste do metodo de remover musica da playList

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/PlaylistControllerTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controller/PlaylistControllerTest.java
@@ -1,9 +1,10 @@
 package com.ciandt.summit.bootcamp2022.controller;
 
-import com.ciandt.summit.bootcamp2022.controller.PlaylistController;
 import com.ciandt.summit.bootcamp2022.controller.dto.MusicaDto;
 import com.ciandt.summit.bootcamp2022.entity.Artista;
 import com.ciandt.summit.bootcamp2022.entity.Musica;
+import com.ciandt.summit.bootcamp2022.exceptions.MusicaNaoEncontradaException;
+import com.ciandt.summit.bootcamp2022.exceptions.PlayListNaoEncontradaException;
 import com.ciandt.summit.bootcamp2022.service.PlayListService;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,8 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class PlaylistControllerTest {
@@ -31,7 +30,6 @@ class PlaylistControllerTest {
 
     @Mock
     private PlayListService playListService;
-
 
 
     @BeforeEach
@@ -76,8 +74,37 @@ class PlaylistControllerTest {
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
-
         assertNotNull(response.getBody());
+    }
+
+    @Test
+    void whenRemoveMusicFromPlayListThenReturnStatusCodeOK(){
+
+        when(playListService.removerMusicaNaPlayList("123", "123")).thenReturn("Música 123 removida da Playlist com sucesso!");
+        ResponseEntity<String> response = playlistController.removerMusicaDaPLaylist("123", "123");
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void whenRemoveMusicFromPlayListThenReturnStringMessageSuccess(){
+
+        when(playListService.removerMusicaNaPlayList("123", "123")).thenReturn("Música 123 removida da Playlist com sucesso!");
+        ResponseEntity<String> response = playlistController.removerMusicaDaPLaylist("123", "123");
+
+        assertEquals("Música 123 removida da Playlist com sucesso!", response.getBody());
+    }
+
+    @Test
+    void whenRemoveMusicFromPlayListWithIdMusicNotFoundAtDataBase(){
+        when(playListService.removerMusicaNaPlayList("123456", "123")).thenThrow(MusicaNaoEncontradaException.class);
+        assertThrows(MusicaNaoEncontradaException.class, () -> playlistController.removerMusicaDaPLaylist("123456", "123"));
+    }
+
+    @Test
+    void whenRemoveMusicFromPlayListWithIdPlayListNotFoundAtDataBase(){
+        when(playListService.removerMusicaNaPlayList("123", "123456")).thenThrow(PlayListNaoEncontradaException.class);
+        assertThrows(PlayListNaoEncontradaException.class, () -> playlistController.removerMusicaDaPLaylist("123", "123456"));
     }
 
 }
